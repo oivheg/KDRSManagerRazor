1e71428be9d7648cf7d5a54e63c77c2f4bd21dc8
Øivind Heggland <oivind@kdrstavanger.no>
2018-09-03 09:13:21 +0200

    -- changed date FRom & To to us Local storage -- added foreach for index resposne, so it shows all Companies.

---
+++ KDRSManagerRazor/wwwroot/js/Global.js
@@ -0,0 +1,19 @@
+﻿//Karina
+var karvar = 5;
+console.log(karvar);
+console.log(getDate("from"));
+
+$(document).ready(function () {
+    console.log('global ready');
+    console.log(karvar);
+});
+
+function saveDate(type, date) {
+    if (typeof (Storage) !== "undefined") {
+        localStorage.setItem(type, date);
+    }
+}
+function getDate(type) {
+    var date = localStorage.getItem(type);
+    return date;
+}
